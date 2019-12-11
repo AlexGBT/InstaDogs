59dcc09dc6e8a4e10a5756609d6216986a5f109e
AlexGBT <alex.berest97@gmail.com>
2019-12-11 08:50:17 +0300

    Registration was created

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,11 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'breed' => ['required', 'string', 'max:255'],
+            'login' => ['required', 'string', 'max:255', 'min:3', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+
         ]);
     }
 
@@ -64,8 +67,10 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+         return User::create([
             'name' => $data['name'],
+            'breed' => $data['breed'],
+            'login' => $data['login'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
